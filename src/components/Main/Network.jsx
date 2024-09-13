9c77c543c968708b8bf15bbaf94d08e3acfdb02e
Valentave <valentintaverna99@gmail.com>
2024-09-12 23:45:06 -0300

    Fix contact href redirection

--- src/components/Main/Network.jsx
+++ src/components/Main/Network.jsx
@@ -4,7 +4,7 @@ export default function Network({item}) {
 
     return(
         <li className="networks-item">
-            <a href={item.name} target="_blank" rel="noreferrer" className={"networks-link link-" + item.name}>
+            <a href={item.link} target="_blank" rel="noreferrer" className={"networks-link link-" + item.name}>
                 <svg viewBox={item.svg.viewBox} className={"networks-svg svg-" + item.name}>
                     {item.svg.path.map((path) => (
                         <path key={path.d} d={path.d}></path>
